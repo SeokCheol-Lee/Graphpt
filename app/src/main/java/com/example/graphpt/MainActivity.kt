6a2035f537cdb2de2facc7b7d3e23930d63c2f3e
lsc <lsc991123@gmail.com>
2022-07-12 15:49:28 +0900

    first commit

---
+++ app/src/main/java/com/example/graphpt/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.example.graphpt
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var barChart: BarChart = findViewById(R.id.chart_week)
+        setChartView(barChart)
+
+    }
+    private fun setChartView(view: View) {
+        var chartWeek = view.findViewById<BarChart>(R.id.chart_week)
+        setWeek(chartWeek)
+    }
+
+    private fun initBarDataSet(barDataSet: BarDataSet) {
+        //Changing the color of the bar
+        barDataSet.color = Color.parseColor("#304567")
+        //Setting the size of the form in the legend
+        barDataSet.formSize = 15f
+        //showing the value of the bar, default true if not set
+        barDataSet.setDrawValues(false)
+        //setting the text size of the value of the bar
+        barDataSet.valueTextSize = 12f
+    }
+
+    private fun setWeek(barChart: BarChart) {
+        initBarChart(barChart)
+
+        barChart.setScaleEnabled(false) //Zoom In/Out
+
+        val valueList = ArrayList<Int>()
+        val entries: ArrayList<BarEntry> = ArrayList()
+        val title = "걸음 수"
+
+        //input data
+        /*
+        for (i in 0..5) {
+            valueList.add(i * 100.1)
+        }
+         */
+        valueList.add(100)
+        valueList.add(900)
+        valueList.add(200)
+        valueList.add(700)
+        valueList.add(100)
+        valueList.add(300)
+        valueList.add(400)
+        valueList.add(100)
+        valueList.add(900)
+        valueList.add(200)
+        valueList.add(700)
+        valueList.add(100)
+        valueList.add(300)
+        valueList.add(400)
+        valueList.add(100)
+        valueList.add(900)
+        valueList.add(200)
+        valueList.add(700)
+        valueList.add(100)
+        valueList.add(300)
+        valueList.add(400)
+
+
+        //fit the data into a bar
+        for (i in 0 until valueList.size) {
+            val barEntry = BarEntry(i.toFloat(), valueList[i].toFloat())
+            entries.add(barEntry)
+        }
+        val barDataSet = BarDataSet(entries, title)
+        val data = BarData(barDataSet)
+        barChart.data = data
+        barChart.invalidate()
+    }
+
+    private fun initBarChart(barChart: BarChart) {
+        //hiding the grey background of the chart, default false if not set
+        barChart.setDrawGridBackground(false)
+        //remove the bar shadow, default false if not set
+        barChart.setDrawBarShadow(false)
+        //remove border of the chart, default false if not set
+        barChart.setDrawBorders(false)
+
+        //remove the description label text located at the lower right corner
+        val description = Description()
+        description.setEnabled(false)
+        barChart.setDescription(description)
+
+        //X, Y 바의 애니메이션 효과
+        barChart.animateY(1000)
+        barChart.animateX(1000)
+
+
+        //바텀 좌표 값
+        val xAxis: XAxis = barChart.getXAxis()
+        //change the position of x-axis to the bottom
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        //set the horizontal distance of the grid line
+        xAxis.granularity = 1f
+        xAxis.textColor = Color.BLACK
+        //hiding the x-axis line, default true if not set
+        xAxis.setDrawAxisLine(false)
+        //hiding the vertical grid lines, default true if not set
+        xAxis.setDrawGridLines(false)
+
+
+        //좌측 값 hiding the left y-axis line, default true if not set
+        val leftAxis: YAxis = barChart.getAxisLeft()
+        leftAxis.setDrawAxisLine(false)
+        leftAxis.textColor = Color.BLACK
+
+
+        //우측 값 hiding the right y-axis line, default true if not set
+        val rightAxis: YAxis = barChart.getAxisRight()
+        rightAxis.setDrawAxisLine(false)
+        rightAxis.textColor = Color.BLACK
+
+
+        //바차트의 타이틀
+        val legend: Legend = barChart.getLegend()
+        //setting the shape of the legend form to line, default square shape
+        legend.form = Legend.LegendForm.LINE
+        //setting the text size of the legend
+        legend.textSize = 18f
+        legend.textColor = Color.BLACK
+        //setting the alignment of legend toward the chart
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+        //setting the stacking direction of legend
+        legend.orientation = Legend.LegendOrientation.HORIZONTAL
+        //setting the location of legend outside the chart, default false if not set
+        legend.setDrawInside(false)
+    }
+
+}
